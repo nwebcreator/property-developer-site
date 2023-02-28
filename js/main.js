21c6b2f884de9482dbb571cdec8c3a2ee2c65598
Natalya <76129369+nwebcreator@users.noreply.github.com>
2023-02-28 16:15:50 +0300

    video block

--- js/main.js
+++ js/main.js
@@ -26,3 +26,22 @@ $(document).ready(function () {
 		},
 	});
 });
+
+/* Video */
+const videoBtn = document.querySelector('#video-btn');
+const videoPicture = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
+
+videoWrapper.addEventListener('click', function () {
+    if (video.paused) {
+        videoPicture.classList.add('hidden');
+		videoWrapper.classList.remove('video-overlay');
+		videoBtn.classList.add('none');
+		video.play();
+	} else {
+		videoWrapper.classList.add('video-overlay');
+		videoBtn.classList.remove('none');
+		video.pause();
+	}
+});
