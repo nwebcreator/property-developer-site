9da383e451f5dd1cf544c07e1cedc10265f8dc75
Natalya <gruzdevanatalya5@gmail.com>
2023-11-08 00:01:03 +0300

    additional changes

--- js/main.js
+++ js/main.js
@@ -45,3 +45,18 @@ videoWrapper.addEventListener('click', function () {
 		video.pause();
 	}
 });
+
+/* Mobile Nav */
+const openNavbtn = document.querySelector('#openMobileNav');
+const closeNavbtn = document.querySelector('#closeMobileNav');
+const mobileNav = document.querySelector('#mobileNav');
+
+openNavbtn.onclick = function () {
+    mobileNav.classList.remove('none')
+    document.body.classList.add('no-scroll');
+}
+
+closeNavbtn.onclick = function () {
+	mobileNav.classList.add('none');
+	document.body.classList.remove('no-scroll');
+};
